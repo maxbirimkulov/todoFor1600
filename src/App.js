da34df4ce1b0a1b68490ce5588975ddd8783a77b
Maksat Birimkulov <maxbirimkulov@mail.ru>
2021-10-29 18:03:26 +0600

    todo3

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 import './style.css'
 import Add from "./components/Add/Add";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbtack, faStickyNote , faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faThumbtack, faStickyNote, faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
 
 
 function App() {
-    const [todoArr, setTodoArr ] =  useState([]);
+    const [todoArr, setTodoArr] = useState([]);
     const [todo, setTodo] = useState('');
 
     let date = new Date();
-    let weekDays =  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const toDate = (date) => {
         return new Intl.DateTimeFormat('en-En', {
-            day:'2-digit',
-            month:'short',
-            year:'numeric',
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
         }).format(new Date(date))
     };
 
     const handlerComplete = (id) => {
-        setTodoArr(todoArr.map((item,idx)=>{
-            if (item.id === id){
+        setTodoArr(todoArr.map((item, idx) => {
+            if (item.id === id) {
                 return {...item, isCompleted: !item.isCompleted}
-            }else{
-                return item
+            } else {
+                return {...item, option: false}
             }
         }))
     };
 
     const handlerOption = (id) => {
-        setTodoArr(todoArr.map((item)=>{
-            if (item.id === id){
-                return {...item, option: !item.option}
-            }else{
-                return item
+        setTodoArr(todoArr.map((item) => {
+            if (item.id === id) {
+                return {...item, option: !item.option, addMemo: false, isChange: false}
+            } else {
+                return {...item, option: false}
             }
         }))
     };
 
     const handlerEdit = (id) => {
-        setTodoArr(todoArr.map((item)=>{
-            if (item.id === id){
-                return {...item, isChange: !item.isChange}
-            }else{
+        setTodoArr(todoArr.map((item) => {
+            if (item.id === id) {
+                return {...item, isChange: !item.isChange, addMemo:false}
+            } else {
                 return item
             }
         }))
@@ -52,67 +52,115 @@ function App() {
 
 
     const handlerDelete = (id) => {
-        setTodoArr(todoArr.filter((item)=>{
+        setTodoArr(todoArr.filter((item) => {
             return item.id !== id
         }))
     };
-  return (
-    <div className="App">
-        <div className="todo">
-            <div>
-                <p>{weekDays[date.getDay()]}</p>
-                <p>{toDate(date)}</p>
-            </div>
-            <Add todo={todo} setTodo={setTodo} setTodoArr={setTodoArr} todoArr={todoArr}/>
-            <ul className='todo__menu'>
-                {todoArr.map((item,idx)=>{
-                    return (
-                        <li className='todo__list' key={item.id}>
-                            <div className='todo__list-left'>
-                               <input className='todo__list-input' type="checkbox" checked={item.isCompleted} onChange={()=> handlerComplete(item.id)}/>
-                               <div className='todo__list-text'>
-                                   {item.isChange
-                                       ? <textarea className='todo__list-textarea' maxLength={30} onChange={(e)=>{
-                                           item.name = e.target.value
-                                       }} >{item.name}</textarea>
-                                       : <span className='todo__list-name'>{item.name}</span>
-                                   }
-                                   <span className='todo__list-memo'>{item.memo}</span>
-                               </div>
 
+    const handlerMemo = (id) => {
+        setTodoArr(todoArr.map((item, idx) => {
+            if (item.id === id) {
+                return {...item, addMemo: !item.addMemo, isChange:false}
+            } else {
+                return item
+            }
+        }))
+    };
 
+    const handlerPin = (id) => {
+        setTodoArr(todoArr.map((item,idx)=>{
+            if (item.id === id){
+                return {...item, pin: !item.pin, option: false}
+            } else{
+                return item
+            }
+        }))
+    };
+    const last = todoArr.length !== 0 ? todoArr.filter((item)=> item.pin).reduce((acc, rec)=>{
+        return rec
+    },{}) : '';
 
-                            </div>
-                            <p className='todo__list-option' onClick={()=> handlerOption(item.id)}>...</p>
-                           <ul className={`todo__list-options ${item.option ? 'active' : ''}`}>
+    return (
+        <div className="App">
+            <div className="todo">
+                <div>
+                    <p>{weekDays[date.getDay()]}</p>
+                    <p>{toDate(date)}</p>
+                </div>
+                <Add todo={todo} setTodo={setTodo} setTodoArr={setTodoArr} todoArr={todoArr}/>
+                <ul className='todo__menu'>
+                    {todoArr.map((item, idx, array) => {
+                        return (
+                            <li
+                                id={last.id === item.id ? 'last': ''}
 
-                               <li className='todo__list-options-item'>
-                                   <FontAwesomeIcon icon={faThumbtack} />
-                                   Pin on the top</li>
+                                className={`todo__list ${item.pin ? 'pin' : ''}`}
+                                key={item.id}>
 
-                               <li className='todo__list-options-item'>
-                                   <FontAwesomeIcon icon={faStickyNote} />
-                                   Add a memo</li>
+                                {item.pin
+                                ? <span className='todo__list-pin'> <FontAwesomeIcon icon={faThumbtack}/></span>
+                                : ''
+                                }
+                                <div className='todo__list-left'>
+                                    <input className='todo__list-input' type="checkbox" checked={item.isCompleted}
+                                           onChange={() => handlerComplete(item.id)}/>
+                                    <div className='todo__list-text'>
+                                        {item.isChange
+                                            ?
+                                            <textarea id='changeName' className='todo__list-textarea' maxLength={30} onChange={(e) => {
+                                                item.name = e.target.value
+                                            }}>{item.name}</textarea>
+                                            : <span className='todo__list-name'>{item.name}</span>
+                                        }
 
-                               <li className='todo__list-options-item' onClick={()=>handlerEdit(item.id)}>
-                                   <FontAwesomeIcon icon={faEdit} />
-                                   {item.isChange
+                                        {item.addMemo
+                                            ? <textarea className='todo__list-memo-textarea' id='changeMemo' onChange={(e)=>{
+                                                item.memo = e.target.value
+                                            }}>{item.memo}</textarea>
+                                            : <span className='todo__list-memo'>{item.memo}</span>
+                                        }
+                                    </div>
+                                </div>
+                                <p className='todo__list-option' onClick={() => handlerOption(item.id)}>...</p>
+                                <ul className={`todo__list-options ${item.option ? 'active' : ''}`}>
+                                    <li className='todo__list-options-item' onClick={()=> handlerPin(item.id)}>
+                                        <FontAwesomeIcon icon={faThumbtack}/>
+                                        {item.pin
+                                        ? 'Unpin'
+                                        : 'Pin on the top'
+                                        }
+                                    </li>
+
+                                    <li className='todo__list-options-item' onClick={() => handlerMemo(item.id)}>
+                                        <FontAwesomeIcon icon={faStickyNote}/>
+                                        {item.addMemo
                                         ? 'Save'
-                                        : 'Edit'
-                                   }
-                               </li>
+                                        : item.memo.length !== 0
+                                            ? 'Edit memo'
+                                            : 'Add a memo'
+                                        }
+                                    </li>
 
-                               <li className='todo__list-options-item' onClick={()=> handlerDelete(item.id)}>
-                                   <FontAwesomeIcon icon={faTrashAlt} />
-                                   Delete</li>
-                           </ul>
-                        </li>
-                    )
-                })}
-            </ul>
+                                    <li className='todo__list-options-item' onClick={() => handlerEdit(item.id)}>
+                                        <FontAwesomeIcon icon={faEdit}/>
+                                        {item.isChange
+                                            ? 'Save'
+                                            : 'Edit'
+                                        }
+                                    </li>
+
+                                    <li className='todo__list-options-item' onClick={() => handlerDelete(item.id)}>
+                                        <FontAwesomeIcon icon={faTrashAlt}/>
+                                        Delete
+                                    </li>
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -7,20 +7,28 @@ const Add = ({todo, setTodo, setTodoArr, todoArr}) => {
             name:todo,
             isCompleted:false,
             isChange:false,
-            isImportant:false,
+            pin:false,
             isActive:true,
             option:false,
+            addMemo:false,
             memo:'',
             id: Math.floor(Math.random() * 10000)
         }]);
         setTodo('')
     };
 
+    const handlerAdd = (e) => {
+        setTodo(e.target.value);
+        setTodoArr(todoArr.map((item)=>{
+            return {...item, option: false}
+        }))
+    }
+
     return (
         <div className='todo__add'>
             <form className='todo__left' onSubmit={addTodo}>
                 <label>
-                    <input className='todo__add-input' maxLength={30} value={todo} type="text" onChange={(e)=> setTodo(e.target.value)} required/>
+                    <input className='todo__add-input' maxLength={30} value={todo} type="text" onChange={handlerAdd} required/>
                 </label>
                 <button type='submit' className='todo__add-btn' >+</button>
             </form>
@@ -28,6 +36,8 @@ const Add = ({todo, setTodo, setTodoArr, todoArr}) => {
             <div className='todo__right'>
                 <select className='todo__add-select' name="" id="">
                     <option> all</option>
+                    <option> completed</option>
+                    <option> important</option>
                 </select>
             </div>
         </div>
@@ -35,3 +45,7 @@ const Add = ({todo, setTodo, setTodoArr, todoArr}) => {
 };
 
 export default Add;
+
+
+
+
